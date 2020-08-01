4cce721c7c9fd0d04170fd6165abb1486a1e1987
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 19:53:00 -0400

    Fatory method para dollar: se realizan los cambios en test

--- src/test/java/edu/globant/tddbyexample/MoneyTest.java
+++ src/test/java/edu/globant/tddbyexample/MoneyTest.java
@@ -9,9 +9,9 @@ public class MoneyTest {
 
     @Test
     public void testMultiplicationDollar() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Dollar five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
