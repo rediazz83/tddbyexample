ef1fe72990a5887820e0087d961e9b095e706d17
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 15:31:54 -0400

    Modificamos la prueba dado el escenario descubierto. Objeto inmutable

--- src/test/java/edu/globant/tddbyexample/MoneyTest.java
+++ src/test/java/edu/globant/tddbyexample/MoneyTest.java
@@ -9,10 +9,10 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
 
-        five.times(3);
-        assertEquals(15, five.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
