120faf359986b560c49998b79865a2c58e7ae538
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 18:20:15 -0400

    Introducimos test correspondientes a franc, lo mismo que para dollar, pero cambiamos la clase

--- src/test/java/edu/globant/tddbyexample/MoneyTest.java
+++ src/test/java/edu/globant/tddbyexample/MoneyTest.java
@@ -22,4 +22,20 @@ public class MoneyTest {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
     }
+
+    @Test
+    public void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(8));
+    }
 }
