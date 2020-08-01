0131d7731e0470b752b561e001587d99e51cdc73
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 14:36:16 -0400

    Olvidé construir el objeto <five> con el numero 5 como argumento. Corregido

--- src/test/java/edu/globant/tddbyexample/MoneyTest.java
+++ src/test/java/edu/globant/tddbyexample/MoneyTest.java
@@ -8,7 +8,7 @@ public class MoneyTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar();
+        Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
     }
