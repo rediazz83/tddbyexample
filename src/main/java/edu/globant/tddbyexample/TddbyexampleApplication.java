829d589b14ab460bf76c2ea5315732d8ad2fb111
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 14:15:57 -0400

    First commit. Base code

---
+++ src/main/java/edu/globant/tddbyexample/TddbyexampleApplication.java
@@ -0,0 +1,13 @@
+package edu.globant.tddbyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddbyexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddbyexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/globant/tddbyexample/TddbyexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.globant.tddbyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddbyexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
