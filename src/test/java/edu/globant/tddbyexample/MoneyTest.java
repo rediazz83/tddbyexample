fdd763135a54511c597bea0efda20cca9e01eb8d
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 18:17:01 -0400

    Renombramos metodos de prueba que solo corresponden a dollar

--- src/test/java/edu/globant/tddbyexample/MoneyTest.java
+++ src/test/java/edu/globant/tddbyexample/MoneyTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    public void testMultiplication() {
+    public void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
@@ -18,7 +18,7 @@ public class MoneyTest {
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
     }
