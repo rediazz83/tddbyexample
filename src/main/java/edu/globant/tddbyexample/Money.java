9cf14a1de30cc628f8ec19d876b2088ee4b5d454
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 19:53:24 -0400

    Fatory method para dollar: se introduce metodo en money

--- src/main/java/edu/globant/tddbyexample/Money.java
+++ src/main/java/edu/globant/tddbyexample/Money.java
@@ -4,6 +4,10 @@ public abstract class Money {
 
     protected int amount;
 
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
