0ec8d86684936397f06b4d238ba7edae245109ab
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-01 19:56:32 -0400

    Refactor: metodo <times> pasa a ser abstracto

--- src/main/java/edu/globant/tddbyexample/Money.java
+++ src/main/java/edu/globant/tddbyexample/Money.java
@@ -4,6 +4,8 @@ public abstract class Money {
 
     protected int amount;
 
+    public abstract Money times(int multiplier);
+
     public static Dollar dollar(int amount){
         return new Dollar(amount);
     }
