6862e8b7d1f731b28495101895f2fb143b656902
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-02 15:01:47 -0400

    Refactor: Solo se ordenan miembros de clase

--- src/main/java/edu/globant/tddbyexample/Money.java
+++ src/main/java/edu/globant/tddbyexample/Money.java
@@ -10,20 +10,20 @@ public class Money {
         this.currency = currency;
     }
 
-    public Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
-    }
-
-    public String currency() {
-        return currency;
-    }
-
     public static Money dollar(int amount){
         return new Money(amount, "USD");
     }
 
     public static Money franc(int amount){
         return new Money(amount, "CHF");
+    }
+
+    public String currency() {
+        return currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 
     @Override
